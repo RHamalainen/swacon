e8edb9f029d51b689a370b0359d40109f10a93cf
Roni Hämäläinen <roni.hamalainen@tuni.fi>
2024-02-14 13:26:43 +0200

    Add __eq__ method for DroneState

--- src/swacon/data_structures/drone/state.py
+++ src/swacon/data_structures/drone/state.py
@@ -1,3 +1,6 @@
+from typing import Any
+
+
 class DroneState:
     def __init__(self, x: float, y: float, z: float, qx: float, qy: float, qz: float, qw: float, angle: float) -> None:
         assert isinstance(x, float)
@@ -21,3 +24,30 @@ class DroneState:
         self.qw = qw
         # Angle in degrees
         self.angle = angle
+
+    def __eq__(self, other: Any) -> bool:
+        if not isinstance(other, DroneState):
+            return False
+        if self.x != other.x:
+            return False
+        if self.y != other.y:
+            return False
+        if self.z != other.z:
+            return False
+        if self.vx != other.vx:
+            return False
+        if self.vy != other.vy:
+            return False
+        if self.vz != other.vz:
+            return False
+        if self.qx != other.qx:
+            return False
+        if self.qy != other.qy:
+            return False
+        if self.qz != other.qz:
+            return False
+        if self.qw != other.qw:
+            return False
+        if self.angle != other.angle:
+            return False
+        return True
