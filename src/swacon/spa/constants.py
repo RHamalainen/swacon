b0849c44ee222263a596b85057dcf7ef234f3b05
Roni Hämäläinen <roni.hamalainen@tuni.fi>
2024-02-14 12:56:10 +0200

    Fix linter warnings

--- src/swacon/spa/constants.py
+++ src/swacon/spa/constants.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
-from swacon.configuration.radio import *
-from swacon.configuration.environment import *
-from swacon.configuration.motion_capture import *
+from swacon.configuration.radio import *  # noqa: F401, F403
+from swacon.configuration.environment import *  # noqa: F401, F403
+from swacon.configuration.motion_capture import *  # noqa: F401, F403
 
 PATH_OUTPUT_ENVIRONMENT_FOLDER = Path("./output/environment/").resolve()
 PATH_OUTPUT_FLIGHT_DATA_FOLDER = Path("./output/flight-data/").resolve()
