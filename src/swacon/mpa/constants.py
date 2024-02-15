058c7ff763b3fc1cf7e56056db4e536d6983e537
Roni Hämäläinen <roni.hamalainen@tuni.fi>
2024-02-15 13:17:57 +0200

    Create constants.py for MPA

---
+++ src/swacon/mpa/constants.py
@@ -0,0 +1,3 @@
+from swacon.configuration.radio import *  # noqa: F401, F403
+from swacon.configuration.environment import *  # noqa: F401, F403
+from swacon.configuration.motion_capture import *  # noqa: F401, F403
