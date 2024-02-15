4d27f3b84ccf83e83a5b90a9c48b3b84882f33fd
Roni Hämäläinen <roni.hamalainen@tuni.fi>
2024-02-15 14:24:42 +0200

    Fix linter warnings

--- src/swacon/mpa/services/common/service_configuration.py
+++ src/swacon/mpa/services/common/service_configuration.py
@@ -4,8 +4,8 @@
 
 import logging
 
-logging.basicConfig(level=logging.INFO)
-
+# TODO: maybe use path.global_var format
 from rpyc.core.protocol import DEFAULT_CONFIG
 
+logging.basicConfig(level=logging.INFO)
 DEFAULT_CONFIG["allow_pickle"] = True
